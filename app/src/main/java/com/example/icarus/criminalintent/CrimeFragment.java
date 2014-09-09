1f0139db776d07d1954b49f0cb54eb0023a2155b
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-09 10:44:34 +0100

    overriding fragment.oncreate overriding oncreate

--- app/src/main/java/com/example/icarus/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/icarus/criminalintent/CrimeFragment.java
@@ -2,6 +2,9 @@ package com.example.icarus.criminalintent;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Icarus on 9/9/2014.
@@ -13,5 +16,12 @@ public class CrimeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(Bundle savedInstanceState);
         mCrime = new Crime();
-    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+            Bundle savedInstanceState){
+            view v = inflater.inflate(R.layout.fragment_crime, parent, false);
+            return v;
+        }
+    
 }
