c2e80ffeb563a7e6ba1c97537d593212a0883fc8
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-09 10:14:29 +0100

    adding getters and setters

--- app/src/main/java/com/example/icarus/criminalintent/Crime/java.java
+++ app/src/main/java/com/example/icarus/criminalintent/Crime/java.java
@@ -12,5 +12,17 @@ public class java {
     public Crime() {
         //Generate unique identifier
         mId = UUID.randomUUID();
+
+        public UUID getId() {
+        return mId;
+    }
+
+        public String getTitle() {
+            return mTitle;
+        }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
     }
 }
