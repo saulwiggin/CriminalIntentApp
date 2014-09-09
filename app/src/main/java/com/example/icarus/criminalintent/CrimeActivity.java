8545449f79397ec5c51a74fa4ad07d46aff8970e
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-09 10:11:25 +0100

    tweaking template code

--- app/src/main/java/com/example/icarus/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/icarus/criminalintent/CrimeActivity.java
@@ -14,14 +14,6 @@ public class CrimeActivity extends Activity {
         setContentView(R.layout.activity_crime);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.crime, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
